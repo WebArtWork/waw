24009edc0ceb6219d141adf32290bb5a6fc3cab6
Honchar Denys <crackeraki@gmail.com>
2018-02-27 16:07:05 +0200

    fixed waw fuw

--- i.js
+++ i.js
@@ -1,9 +1,3 @@
 module.exports = function(sd, cb){
 	sd._cmd.get('npm i', cb);
-	sd._cmd.get('pwd', function(err, body){
-		console.log('err123');
-		console.log(err);
-		console.log('body');
-		console.log(body);
-	});
 }
