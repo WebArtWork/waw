013b193d1b86daf71e38a2379ee9955ca1d76173
Honchar Denys <crackeraki@gmail.com>
2020-02-01 17:56:02 +0200

    added read config

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
-
-//require(__dirname+'/runners')(__dirname);
-
+const fs = require('fs');
+let config = {};
+if (fs.existsSync(process.cwd()+'/config.json')) {
+	config = JSON.parse(fs.readFileSync(process.cwd()+'/config.json'));
+	fs.mkdirSync(process.cwd()+'/client', { recursive: true });
+	fs.mkdirSync(process.cwd()+'/server', { recursive: true });
+}
+if (fs.existsSync(process.cwd()+'/server.json')) {
+	let serverConfig = fs.readFileSync(process.cwd()+'/server.json');
+	for(let each in serverConfig){
+		config[each] = serverConfig[each];
+	}
+}
+console.log(config);
+process.exit(1);
 /*
-Read config
 Read local parts
 From config read/install common parts
 Execute runners
 Require routers
 */
+//require(__dirname+'/runners')(__dirname);
